acaf7635a23ce1eb67cbe3e8db1342df3f34812f
Junaid <junaid@naumans-imac.local>
2019-07-29 14:03:51 +0500

    Initial Commit

---
+++ TestingSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingSwift
+//
+//  Created by Junaid on 7/29/19.
+//  Copyright © 2019 Mamango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
